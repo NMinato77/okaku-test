b0c801d4657355ec823914430bc1eb8646312ad2
Minato Yamashita <36177990+NMinato77@users.noreply.github.com>
2024-06-03 13:27:19 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch('https://minatoyamashita.com/okaku-test/process', {
+    const response = await fetch('https://NMinato77.github.io/okaku-test/process', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
